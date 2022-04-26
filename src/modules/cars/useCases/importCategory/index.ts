7e15132768a8a7db3a913fc7070d0137fc9da16a
Leandro Lelis <leandroaugustolelis@gmail.com>
2022-04-25 22:14:38 -0300

    refactoring category use case

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/implementations/Categor
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const categorysRepository = CategoriesRepository.getInstance();
+const categorysRepository = null;
 
 const importCategoryUseCase = new ImportCategoryUseCase(categorysRepository);
 
