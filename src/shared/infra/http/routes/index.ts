360514ccdf8958c797ac91667b72efce358d01de
Leandro Lelis <leandroaugustolelis@gmail.com>
2022-09-28 03:26:06 -0300

    list available cars by name, brand, category_id [ tests and development done ]

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -11,7 +11,7 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
-router.use("/car", carRoutes);
+router.use("/cars", carRoutes);
 router.use(authenticateRoutes);
 
 export { router };
