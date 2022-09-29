1f2f8494e0596669c8f27d46d8a991e59d959d74
Leandro Lelis <leandroaugustolelis@gmail.com>
2022-09-29 15:35:05 -0300

    creation of rentals migration

--- src/shared/infra/typeorm/migrations/1651071611920-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1651071611920-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1651071611920 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
